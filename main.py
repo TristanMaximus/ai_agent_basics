7308cf987a94c16d6c8fcacba0ca2b94006b171c
TristanMaximus <tristan.minsk@gmail.com>
2026-02-25 23:23:40 +0100

    [CH1-L6] Multiple messages
    Added support for multiple messages to send to LLM

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     # Now we can access `args.user_prompt`
     print("Hello from ai-agent-basics!")
     load_dotenv()
@@ -14,7 +16,7 @@ def main():
     if api_key == None:
         raise RuntimeError("Error: Google Gemini API Key is not present!")
     gemini_client = genai.Client(api_key = api_key)
-    gemini_response = gemini_client.models.generate_content(model="gemini-2.5-flash", contents = args.user_prompt)
+    gemini_response = gemini_client.models.generate_content(model="gemini-2.5-flash", contents = messages)
     usage_metadata = gemini_response.usage_metadata
     if usage_metadata != None:
         print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
