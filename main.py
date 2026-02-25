1506296139262e02d607c8b1f78f0e07d2a64cbb
TristanMaximus <tristan.minsk@gmail.com>
2026-02-25 23:13:55 +0100

    [CH1-L4] Token Metadata
    Extracted number of tokens in prompt and in response to the console log

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def main():
         raise RuntimeError("Error: Google Gemini API Key is not present!")
     gemini_client = genai.Client(api_key = api_key)
     gemini_response = gemini_client.models.generate_content(model="gemini-2.5-flash", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    usage_metadata = gemini_response.usage_metadata
+    if usage_metadata != None:
+        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {usage_metadata.candidates_token_count}")
+    else:
+        raise RuntimeError("Error: No Usage Metadata in Gemini response.")
     print(gemini_response.text)
 
 
