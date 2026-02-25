904220d18e16d17e82ed51f6f213750f5c6b74f7
TristanMaximus <tristan.minsk@gmail.com>
2026-02-25 23:18:59 +0100

    [CH1-L5] User input
    Added console command argument parsing

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
     print("Hello from ai-agent-basics!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("Error: Google Gemini API Key is not present!")
     gemini_client = genai.Client(api_key = api_key)
-    gemini_response = gemini_client.models.generate_content(model="gemini-2.5-flash", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    gemini_response = gemini_client.models.generate_content(model="gemini-2.5-flash", contents = args.user_prompt)
     usage_metadata = gemini_response.usage_metadata
     if usage_metadata != None:
         print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
