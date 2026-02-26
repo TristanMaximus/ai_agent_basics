864d8e263c663e4552a1a7e1fee3001a0bb8a391
TristanMaximus <tristan.minsk@gmail.com>
2026-02-26 23:00:34 +0100

    [CH1-L7] Verbose Output
    Added optional --verbose command line argument to indicate the need for
    more logs

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from google.genai import types
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     # Now we can access `args.user_prompt`
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     print("Hello from ai-agent-basics!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,8 +22,9 @@ def main():
     gemini_response = gemini_client.models.generate_content(model="gemini-2.5-flash", contents = messages)
     usage_metadata = gemini_response.usage_metadata
     if usage_metadata != None:
-        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {usage_metadata.candidates_token_count}")
+        if args.verbose:
+            print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {usage_metadata.candidates_token_count}")
     else:
         raise RuntimeError("Error: No Usage Metadata in Gemini response.")
     print(gemini_response.text)
